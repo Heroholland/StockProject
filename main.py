89a0768b07b866ab962e4157aa58752f05bfc69a
Heroholland <66584510+Heroholland@users.noreply.github.com>
2024-09-24 13:25:07 -0500

    Secrets Changed

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv # Load environment variables from .env file
 
 load_dotenv()
 
-BASE_URL = "https://paper-api.alpaca.markets"
+BASE_URL = os.getenv("BASE_URL")
 ALPACA_API_KEY = os.getenv("ALPACA_API_KEY")
 ALPACA_SECRET_KEY = os.getenv("ALPACA_SECRET_KEY")
 
